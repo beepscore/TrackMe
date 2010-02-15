2609e4dd78f683e04517a02866ede14e44d74bbc
beepscore <support@beepscore.com>
2010-02-15 01:56:56 -0800

    Add frameworks MapKit and CoreLocation. Add PointOfInterest class. Add MapView.

--- Classes/MainViewController.h
+++ Classes/MainViewController.h
@@ -6,10 +6,17 @@
 //  Copyright Beepscore LLC 2010. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 #import "FlipsideViewController.h"
 
-@interface MainViewController : UIViewController <FlipsideViewControllerDelegate> {
+@interface MainViewController : UIViewController 
+<FlipsideViewControllerDelegate, CLLocationManagerDelegate, MKMapViewDelegate> {
+    MKMapView *myMapView;
 }
+#pragma mark -
+#pragma mark properties
+@property(nonatomic, retain) IBOutlet MKMapView *myMapView;
 
 - (IBAction)showInfo;
 
--- Classes/MainViewController.m
+++ Classes/MainViewController.m
@@ -8,9 +8,14 @@
 
 #import "MainViewController.h"
 #import "MainView.h"
+#import "PointOfInterest.h"
 
 
 @implementation MainViewController
+
+#pragma mark -
+#pragma mark properties
+@synthesize myMapView;
 
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
@@ -21,12 +26,36 @@
 }
 
 
-/*
- // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
- - (void)viewDidLoad {
- [super viewDidLoad];
- }
- */
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Load the points of interest. Very manual for now.    
+    // CLLocationCoordinate2D coord1 = { 37.323388, -122.013958 };
+    // Seattle
+    CLLocationCoordinate2D coord1 = { 47.65, -122.35 };
+    PointOfInterest *pointOfInterest1 = [[PointOfInterest alloc] init];
+    pointOfInterest1.coordinate = coord1;
+    pointOfInterest1.title = @"Point 1";
+    
+    CLLocationCoordinate2D coord2 = { 47.55, -122.15 };
+    PointOfInterest *pointOfIntererst2 = [[PointOfInterest alloc] init];
+    pointOfIntererst2.coordinate = coord2;
+    pointOfIntererst2.title = @"Point 2";
+    
+    CLLocationCoordinate2D initialCenter = pointOfInterest1.coordinate;
+    
+    //  MKCoordinateSpan initialSpan = MKCoordinateSpanMake(0.454305, 0.398254);
+    MKCoordinateSpan initialSpan = MKCoordinateSpanMake(0.2, 0.2);
+    
+    MKCoordinateRegion initialRegion = MKCoordinateRegionMake(initialCenter, initialSpan);
+    self.myMapView.region = initialRegion;
+    
+    [self.myMapView addAnnotation:pointOfInterest1];
+    [self.myMapView addAnnotation:pointOfIntererst2];
+    [pointOfInterest1 release], pointOfInterest1 = nil;
+    [pointOfIntererst2 release], pointOfIntererst2 = nil;
+}
 
 
 /*
@@ -65,6 +94,7 @@
  }
  */
 
+
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
@@ -72,15 +102,82 @@
 	// Release any cached data, images, etc that aren't in use.
 }
 
+
+#pragma mark destructors and memory cleanUp
+// use cleanUp method to avoid repeating code in setView, viewDidUnload, and dealloc
+- (void)cleanUp {
+    [myMapView release], myMapView = nil;
+}
+
+
+// Release IBOutlets in setView.  
+// Ref http://developer.apple.com/iPhone/library/documentation/Cocoa/Conceptual/MemoryMgmt/Articles/mmNibObjects.html
+//
+// http://moodle.extn.washington.edu/mod/forum/discuss.php?d=3162
+- (void)setView:(UIView *)aView {
+    
+    if (!aView) { // view is being set to nil        
+        // set outlets to nil, e.g. 
+        // self.anOutlet = nil;
+        [self cleanUp];
+    }    
+    // Invoke super's implementation last    
+    [super setView:aView];    
+}
+
+
 - (void)viewDidUnload {
 	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
+	[self cleanUp];
 }
 
 
 - (void)dealloc {
+    [self cleanUp];
     [super dealloc];
+}
+
+#pragma mark -
+#pragma mark MKMapViewDelegate methods
+// Ref Dudney sec 25.3
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation {
+    
+    MKPinAnnotationView *annotationView = nil;
+    
+    if(annotation != mapView.userLocation)
+    {
+        // Attempt to get an unused annotationView.  Returns nil if one isn't available.
+        // Ref http://developer.apple.com/iphone/library/documentation/MapKit/Reference/MKMapView_Class/MKMapView/MKMapView.html#//apple_ref/occ/instm/MKMapView/dequeueReusableAnnotationViewWithIdentifier:
+        annotationView = (MKPinAnnotationView *)
+        [mapView dequeueReusableAnnotationViewWithIdentifier:@"myIdentifier"];
+        
+        // if dequeue didn't return an annotationView, allocate a new one
+        if (nil == annotationView) {
+            // NSLog(@"dequeue didn't return an annotationView, allocing a new one");
+            annotationView = [[[MKPinAnnotationView alloc] 
+                               initWithAnnotation:annotation
+                               reuseIdentifier:@"myIdentifier"]
+                              autorelease];
+        } else {
+            NSLog(@"dequeueReusableAnnotationViewWithIdentifier returned an annotationView");
+        }    
+        [annotationView setPinColor:MKPinAnnotationColorPurple];
+        [annotationView setCanShowCallout:YES];
+        [annotationView setAnimatesDrop:YES];
+    } else {
+        [mapView.userLocation setTitle:@"I am here"];
+    }
+    return annotationView;
+}
+
+
+- (void)mapView:(MKMapView *)aMapView regionDidChangeAnimated:(BOOL)animated
+{
+    NSLog(@"lat: %f, long: %f, latDelta: %f, longDelta: %f",
+          aMapView.region.center.latitude, aMapView.region.center.longitude, 
+          aMapView.region.span.latitudeDelta, aMapView.region.span.longitudeDelta);
 }
 
 
 @end
+
